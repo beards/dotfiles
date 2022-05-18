9c138cf4261be4d4db2f383578904159d091362f
bear <bear.darkshine@gmail.com>
2022-05-18 16:30:07 +0800

    Update color script

--- scripts/color.sh
+++ scripts/color.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
 if [ -z $1 ]; then
-    BREAK=1
+    BREAK=8
 else
     BREAK=$1
 fi
+BREAK2=$((BREAK / 2))
+echo $BREAK2
+
 for i in {0..255} ; do
     printf "\x1b[38;5;${i}mcolour${i} \t"
     if [ $(( i % $BREAK )) -eq $(($BREAK-1)) ] ; then
         printf "\n"
     fi
 done
+printf "\n"
+
+for i in {0..64} ; do
+    printf "\033[0;${i}m[0;%2d]\033[0m  \t" ${i}
+    printf "\033[1;${i}m[1;%2d]\033[0m  \t" ${i}
+    if [ $(( i % $BREAK2 )) -eq $(($BREAK2-1)) ] ; then
+        printf "\n"
+    fi
+done
+printf "\n"
